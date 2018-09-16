ce3d228fb74ddbece0f5e685c9d3398966131aa0
OlegAnn <olegihimich@gmail.com>
2018-09-16 18:57:29 +0300

    change img

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,7 +1,6 @@
 module.exports = function () {
 	$.gulp.task('sass', function() {
 	 return $.gulp.src($.paths.sass)
-    .pipe($.gp.plumber())
     .pipe($.gp.sourcemaps.init())
 		.pipe($.gp.sass())
 		.pipe($.gp.autoprefixer({
@@ -13,6 +12,7 @@ module.exports = function () {
 		.pipe($.gp.csso())
 		.pipe($.gp.sourcemaps.write())
 		.pipe($.gulp.dest('build/static/css/'))
+    .pipe($.gp.plumber())
 		.pipe($.browserSync.reload({
 			stream:true
 		}));
